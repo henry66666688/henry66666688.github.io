e0806930c56f4587934d1dc88d1b27ab28740bcf
henry66666688 <1149582101@qq.com>
2026-07-18 21:52:20 +0800

    Update public-config.js

--- public-config.js
+++ public-config.js
@@ -2,7 +2,7 @@
 
 window.NANCHONG_PUBLIC_CONFIG = {
   "mode": "tester",
-  "apiBase": "https://americans-cell-wyoming-vegetables.trycloudflare.com",
+  "apiBase": "https://came-spoke-involves-program.trycloudflare.com",
   "testerKey": "2rjfLDsjzGlv5HnY9YwtBuHWMlfPrDEZ",
   "tileAssetRoot": "./assets/tiles/complete-chart-reference-v2/front"
 };
