8413898b036933bd4d28dbd703657ef44a255417
henry66666688 <1149582101@qq.com>
2026-07-19 07:34:28 +0800

    Update public-config.js

--- public-config.js
+++ public-config.js
@@ -2,7 +2,7 @@
 
 window.NANCHONG_PUBLIC_CONFIG = {
   "mode": "tester",
-  "apiBase": "https://came-spoke-involves-program.trycloudflare.com",
+  "apiBase": "https://collection-bingo-streams-spot.trycloudflare.com",
   "testerKey": "2rjfLDsjzGlv5HnY9YwtBuHWMlfPrDEZ",
   "tileAssetRoot": "./assets/tiles/complete-chart-reference-v2/front"
 };
